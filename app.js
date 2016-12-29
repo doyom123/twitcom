05e8c821392e0e7924d786c9183d0a091a961a19
doyom123 <doom@buffalo.edu>
2016-12-29 01:39:16 -0500

    twitter status update tests

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var Oauth = require('oauth').OAuth;
+var Twitter = require('twitter');
+var CronJob = require('cron').CronJob;
+var env = require('dotenv').config();
 var app = express();
 var connection = mysql.createConnection({
 	host: '127.0.0.1',
 	user: 'vagrant',
 	password: '',
 	database: 'twitcom'
+});
+
+var job = new CronJob({
+	cronTime: '00 30 11 * * *',
+	onTicK: function() {
+
+	},
+	start: false,
+	timeZone: 'US/Eastern'
+});
+
+var twitter = new Twitter({
+	consumer_key: env.consumer_key,
+	consumer_secret: env.consumer_secret,
+	access_token_key: env.access_token_key,
+	access_token_secret: env.access_token_secret
 });
 
 connection.connect(function(err) {
@@ -22,6 +42,8 @@ connection.connect(function(err) {
 	app.listen(8080, function() {
 		console.log('Web Server listening on port 8080');
 	});
+	job.start();
+	console.log('CronJob started');
 });
 
 app.set('view engine', 'ejs');
@@ -35,6 +57,7 @@ app.get('/', function(req, res) {
 	connection.query(query, function(err, results) {
 		if(err) {
 			console.log(err);
+			console.log("hi");
 		}
 		for(var i = 0; i < results.length; i++) {
 			var tweet = results[i];
@@ -93,6 +116,23 @@ app.post('/vote/:id([0-9]+)', function(req, res) {
 		res.redirect('/');
 	});
 });
+
+// Test Twitter Update Status
+app.post('/status', function(req, res) {
+
+	twitter.post('statuses/update', {status: 'Testing'},  function(error, tweet, response){
+	  if(error){
+	    console.log(error);
+	  }
+	  console.log(tweet);  // Tweet body.
+	  //console.log(response);  // Raw response object.
+	});
+	res.redirect('/');
+});
+
+// Cron Job
+
+
 // app.post('/', function(req, res) {
 // 	var query = 'UPDATE Tweets SET vote_count = vote_count + 1 WHERE id = 1';
 // 	connection.query(query, function(err, results) {
